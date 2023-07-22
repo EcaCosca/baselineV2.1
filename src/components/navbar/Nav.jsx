45c88e0c081fef4bcd20e6dfac79d156f8a78798
EcaCosca <enriquecoscarelli@gmail.com>
2023-07-22 10:56:18 +0200

    Working navbar

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import logo from '../../assets/5ffb61d99e16ce8b4f3c4e6da8a6d52d-baseline20.svg'
+import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div>
+        <div className='logo'>
+            <img src={logo}/>
+        </div>
+        <ul>
+            <li><NavLink>Tracks</NavLink></li>
+            <li><NavLink>Profiles</NavLink></li>
+            <li><NavLink>Map</NavLink></li>
+        </ul>
+        <div>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </div>
+    </div>
   )
 }
 
