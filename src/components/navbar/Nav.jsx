673ba91a84db255fde8116a91fe99dabde47a286
EcaCosca <enriquecoscarelli@gmail.com>
2023-07-24 18:32:33 +0200

    DDING ICONS

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import logo from '../../assets/5ffb61d99e16ce8b4f3c4e6da8a6d52d-baseline20.svg'
 import { NavLink } from 'react-router-dom'
+import NavIcon from './NavIcon'
+import {BsPlus, BsFillLightningFill,BsGearFill} from 'react-icons/bs'
+import {FaFire, FaPoo} from 'react-icons/fa'
+
 
 const Nav = () => {
   return (
@@ -19,10 +23,14 @@ const Nav = () => {
             <li></li>
             <li></li>
         </div> */}
-        <i>A</i>
+        {/* <i>A</i>
         <i>B</i>
         <i>C</i>
-        <i>D</i>
+        <i>D</i> */}
+        <NavIcon icon={<FaFire size={28}/>}/>
+        <NavIcon icon={<BsPlus size={32}/>}/>
+        <NavIcon icon={<BsFillLightningFill size={20}/>}/>
+        <NavIcon icon={<FaPoo size={20}/>}/>
     </div>
   )
 }
