fde6f613be4d5fc54a0cee5192ab3fe5861a268c
EcaCosca <enriquecoscarelli@gmail.com>
2023-07-24 18:24:57 +0200

    Installing react icons

---
+++ src/components/navbar/NavIcon.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavIcon = ({icon}) => {
+  return (
+    <div className="sidebar-icon">{icon}</div>
+  )
+}
+
+export default NavIcon
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+// import colors from'tailwind/colors'
+
 export default {
   content: [
     "./index.html",
@@ -14,7 +17,19 @@ export default {
     extend: {
       colors: {
         primary: "#202225",
-        secondary: "#5865f2"
+        secondary: "#5865f2",
+        // gray: colors.trueGray,
+        gray: {
+          900: '#202225',
+          800: '#2f3136',
+          700: '#36393f',
+          600: '#4f545c',
+          400: '#d4d7dc',
+          300: '#e3e5e8',
+          200: '#ebedef',
+          100: '#f2f3f5',
+        }
+
       }
     },
   },
