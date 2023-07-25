38822cca6ca0f5853cd8e19b31146f611b864d52
EcaCosca <enriquecoscarelli@gmail.com>
2023-07-25 18:03:32 +0200

    Adding dummy data

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -1,66 +1,98 @@
-import React from 'react'
-import logo from '../../assets/5ffb61d99e16ce8b4f3c4e6da8a6d52d-baseline20.svg'
-import { NavLink } from 'react-router-dom'
-import NavIcon from './NavIcon'
-import {BsPlus} from 'react-icons/bs'
-import {FaHome, FaPaypal} from 'react-icons/fa'
-import {BiStats} from 'react-icons/bi'
-import {LiaMapMarkedAltSolid} from 'react-icons/lia'
-import {FiSettings} from 'react-icons/fi'
-import {AiOutlineMail, AiOutlineShoppingCart} from 'react-icons/ai'
-import {HiDocumentText} from 'react-icons/hi'
-
+import React from "react";
+// TODO Create Logo
+// import logo from "../../assets/5ffb61d99e16ce8b4f3c4e6da8a6d52d-baseline20.svg";
+import { NavLink } from "react-router-dom";
+import NavIcon from "./NavIcon";
+import { BsPlus } from "react-icons/bs";
+import { FaHome, FaPaypal } from "react-icons/fa";
+import { BiStats } from "react-icons/bi";
+import { LiaMapMarkedAltSolid } from "react-icons/lia";
+import { FiSettings } from "react-icons/fi";
+import { AiOutlineMail, AiOutlineShoppingCart } from "react-icons/ai";
+import { HiDocumentText } from "react-icons/hi";
 
 const Nav = () => {
   return (
-    <div className='fixed top-0 left-0 h-screen w-16 mx-auto flex flex-col bg-primary text-secondary shadow-lg'>
+    <div className="fixed top-0 left-0 h-screen w-16 mx-auto flex flex-col bg-primary text-secondary shadow-lg">
       <NavLink to="/">
         {({ isActive, isPending }) => (
-        <NavIcon active={isActive} icon={<FaHome size={28}/>} text="Home" />
+          <NavIcon active={isActive} icon={<FaHome size={28} />} text="Home" />
         )}
       </NavLink>
-        <NavLink to="/dashboard">
+      <NavLink to="/dashboard">
         {({ isActive, isPending }) => (
-        <NavIcon  active={isActive} icon={<BsPlus size={32}/>} text="Dashboard"/>
+          <NavIcon
+            active={isActive}
+            icon={<BsPlus size={32} />}
+            text="Dashboard"
+          />
         )}
       </NavLink>
-        <NavLink to="/profiles">
+      <NavLink to="/profiles">
         {({ isActive, isPending }) => (
-        <NavIcon  active={isActive} icon={<BiStats size={20}/>} text="Profiles"/>
+          <NavIcon
+            active={isActive}
+            icon={<BiStats size={20} />}
+            text="Profiles"
+          />
         )}
       </NavLink>
-        <NavLink to="/map">
+      <NavLink to="/map">
         {({ isActive, isPending }) => (
-        <NavIcon  active={isActive} icon={<LiaMapMarkedAltSolid size={20}/>} text="Map"/>
+          <NavIcon
+            active={isActive}
+            icon={<LiaMapMarkedAltSolid size={20} />}
+            text="Map"
+          />
         )}
       </NavLink>
-        <NavLink to="/settings">
+      <NavLink to="/settings">
         {({ isActive, isPending }) => (
-        <NavIcon  active={isActive} icon={<FiSettings size={20}/>} text="Settings"/>
+          <NavIcon
+            active={isActive}
+            icon={<FiSettings size={20} />}
+            text="Settings"
+          />
         )}
       </NavLink>
-        <NavLink to="/contact">
+      <NavLink to="/contact">
         {({ isActive, isPending }) => (
-        <NavIcon  active={isActive} icon={<AiOutlineMail size={20}/>} text="Contact"/>
+          <NavIcon
+            active={isActive}
+            icon={<AiOutlineMail size={20} />}
+            text="Contact"
+          />
         )}
       </NavLink>
-        <NavLink to="/documentation">
+      <NavLink to="/documentation">
         {({ isActive, isPending }) => (
-        <NavIcon  active={isActive} icon={<HiDocumentText size={20}/>} text="Documentation"/>
+          <NavIcon
+            active={isActive}
+            icon={<HiDocumentText size={20} />}
+            text="Documentation"
+          />
         )}
       </NavLink>
-        <NavLink to="/donate">
+      <NavLink to="/donate">
         {({ isActive, isPending }) => (
-        <NavIcon  active={isActive} icon={<FaPaypal size={20}/>} text="Donate"/>
+          <NavIcon
+            active={isActive}
+            icon={<FaPaypal size={20} />}
+            text="Donate"
+          />
         )}
       </NavLink>
-        <NavLink to="/market">
+      <NavLink to="/market">
         {({ isActive, isPending }) => (
-        <NavIcon  active={isActive} icon={<AiOutlineShoppingCart size={20}/>} text="Market"/>
+          <NavIcon
+            active={isActive}
+            icon={<AiOutlineShoppingCart size={20} />}
+            text="Market"
+          />
         )}
       </NavLink>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
--- src/pages/Tracks.jsx
+++ src/pages/Tracks.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Tracks = () => {
+  const [trackData, setTrackData] = useState([
+    {date:"2023 Jul 14 21:24:52 CEST", height:581, country:"Switzerland", exit:"Nose 3.5", suit:"Corvid", type:"BASE"},
+    {date:"2023 Jul 14 18:31:00 CEST", height:1519, country:"Switzerland", exit:"Low Ultimate", suit:"Corvid", type:"BASE"},
+    {date:"2023 Jul 14 13:34:44 CEST", height:510, country:"Switzerland", exit:"Nose 3", suit:"Corvid", type:"BASE"},
+    {date:"2023 Jul 13 17:19:16 CEST", height:572, country:"Switzerland", exit:"High Nose", suit:"Corvid", type:"BASE"},
+    {date:"2023 Jul 11 18:15:21 CEST", height:646, country:"Switzerland", exit:"Via Ferrata", suit:"Corvid", type:"BASE"},
+    {date:"2023 Jul 10 19:11:02 CEST", height:804, country:"Switzerland", exit:"Nose 3.5", suit:"Corvid", type:"BASE"},
+    {date:"2022 Dec 20 11:47:36 CET", height:881, country:"France", location:"Belvédère	Freak", type:"BASE"},
+    {date:"2022 Nov 27 13:56:09 CET", height:871, country:"Switzerland", location:"Le Châble", type:"BASE"},
+    {date:"2022 Nov 27 13:56:04 CET", height:872, country:"Switzerland", location:"Le Châble", suit:"Freak", type:"BASE"},
+    {date:"2022 Nov 27 11:21:48 CET", height:3440, country:"Switzerland", location:"Verbier", suit:"Freak", type:"Skydive"},
+    {date:"2022 Oct 02 15:53:00 CEST", height:4063, country:"Spain", location:"Saltamos Village", type:"Skydive"},
+    {date:"2022 Oct 02 13:30:42 CEST", height:5285, country:"Spain", location:"Saltamos Village", type:"Skydive"},
+  ])
+
+
   return (
-    <div>Tracks</div>
+    <div>
+      <h3>Tracks</h3>
+
+      {/* FILTERS */}
+      {/* TRACKS */}
+      {/* TOTAL, BASE, SKYDIVE */}
+      
+      {/* COUNTRIES */}
+
+      {/* SUITS */}
+
+
+      {/* LIMIT RESULTS BY QUANTITY */}
+      {/* SEACHBAR */}
+      {/* TABLE */}
+      {/* table header */}
+      {/* date, height, location, suit, BASE */}
+      {/* PAGINATION */}
+    </div>
   )
 }
 
